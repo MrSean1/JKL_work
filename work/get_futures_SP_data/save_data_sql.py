9fe691e83b0e844116f4a8d1fe52cc329717661a
haitao <wht190421@163.com>
2019-03-07 18:01:20 +0800

    finally

--- work/get_futures_SP_data/save_data_sql.py
+++ work/get_futures_SP_data/save_data_sql.py
@@ -32,16 +32,29 @@ def main(table_name, prod_code):
     cur_path = os.getcwd() + os.path.sep + 'data'
     cur_path + os.path.sep + prod_code
     # 前一天写进数据文件
-    file_name = str(datetime.date.today() - datetime.timedelta(days=1)) + '_' + prod_code + '.csv'
-    datetime.timedelta()
-    try:
-        with open(cur_path + os.path.sep + cur_path + os.path.sep + file_name, 'r') as f:
-            msg = f.read()
-        save_data = write_sql_data(msg)
-        save_data_sql = "INSERT INTO {}(type, price, quantity, date_time) VALUES {}".format(table_name, save_data)
-        cursor.execute(save_data_sql)
-    except Exception as e:
-        log.logger.error(str(e))
+    # file_name = str(datetime.date.today() - datetime.timedelta(days=1)) + '_' + prod_code + '.csv'
+    file_name_list = ['2019-02-25_YMH9.csv', '2019-02-26_YMH9.csv', '2019-02-27_YMH9.csv', '2019-02-28_YMH9.csv']
+    for file_name in file_name_list:
+        datetime.timedelta()
+        try:
+            with open(cur_path + os.path.sep + prod_code + os.path.sep + file_name, 'r') as f:
+                msg = f.read()
+            save_data = write_sql_data(msg)
+
+            for data in save_data:
+                save_data_sql = "INSERT INTO {}(type, price, quantity, date_time) VALUES {}".format(table_name, data)
+                try:
+                    cursor.execute(save_data_sql)
+                    db.commit()
+                    log.logger.info('save a data, data:{}'.format(data))
+                except Exception as e:
+                    log.logger.error(e)
+            cursor.close()
+            db.close()
+        except Exception as e:
+            cursor.close()
+            db.close()
+            log.logger.error(str(e))
 
 
 def write_sql_data(msg):
@@ -51,13 +64,13 @@ def write_sql_data(msg):
     """
     data_list = msg.split('\n')[1::]
     data_list = data_list[:-1]
-    save_data = list()
+    # save_data = list()
     for data in data_list:
         save_data_list = data.split(',')
         save_data_tuple = tuple(save_data_list)
-        data.append(save_data_tuple)
-    return tuple(save_data)
+        # data.append(save_data_tuple)
+        yield tuple(save_data_tuple)
 
 
 if __name__ == '__main__':
-    main()
+    main('YM_data', 'YMH9')
